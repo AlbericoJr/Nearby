a90704d433c5dfed79b69d251b77a6c46769196d
AlbericoJr <40373171+AlbericoJr@users.noreply.github.com>
2024-12-11 11:29:15 -0300

    Continuação

---
+++ src/app/home.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return(
+    <View style={{flex: 1}}>
+    </View>
+  )
+}
+
+// https://youtu.be/lsOLhrDEDxw?si=2pEUGEP6j3fL8WL_
